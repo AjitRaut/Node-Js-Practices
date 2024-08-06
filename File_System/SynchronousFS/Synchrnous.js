a41ba2d5f2f04a7c2e07694a56e33a9883a7ef18
Ajit raut <ajitraut9561@gmail.com>
2024-08-06 09:30:00 +0530

    Synchronous Fs Complete

--- File_System/SynchronousFS/Synchrnous.js
+++ File_System/SynchronousFS/Synchrnous.js
@@ -1,20 +1,46 @@
 const fs = require("fs");
+const { json } = require("stream/consumers");
 
+// Writing File In Synchronous File Ststem.
 const writefile = (data) => {
   fs.writeFileSync("data.json", JSON.stringify(data));
 };
 
-writefile({Id: 1 ,name : "Ajit" ,});
+// Raeding File In Synchronous File Ststem.
+const readfile = () => {
+  const data = fs.readFileSync("data.json", "utf8");
+  console.log(data);
+};
 
-// const readfile = () => {
-//   const data = fs.readFileSync("data.json", "utf8");
-//   console.log(data)
-// };
+// Appending File In File System.
+const append_File = () => {
+  const appenddata = {
+    id: 2,
+    name: "Ajit",
+  };
 
-// const deletefile =()=>{
-//     fs.unlinkSync("data.json")
-// } 
+  fs.appendFileSync("data.json", '\n' + JSON.stringify(appenddata) );
+};
 
-// // writefile({ name: "ajit" });
-// // readfile();
+//Updating File In Synchronous File System.
+const update_File = (updated_data) => {
+  if (fs.existsSync("data.json")) {
+    fs.writeFileSync("data.json" , JSON.stringify(updated_data));
+  } else {
+    console.log("File Does not Exist");
+  }
+};
+
+// Deletting File In Synchronous File Syatem.
+const deletefile = () => {
+  fs.unlinkSync("data.json");
+};
+
+writefile({ Id: 1, name: "Ajit" });
+// append_File();
+update_File({id: 2 , name : "ajit"})
+readfile();
 // deletefile();
+
+
+
