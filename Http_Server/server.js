35a8c9dc91dc557f98d1b9518166de0ed8025c46
Ajit raut <ajitraut9561@gmail.com>
2024-08-08 12:06:26 +0530

    Working With Http Server

--- Http_Server/server.js
+++ Http_Server/server.js
@@ -3,8 +3,16 @@
 const http = require("http")
 
 const server = http.createServer((req , res)=>{
- res.write('Wlcome To Our Server ok')
- res.end()
+ if(req.url === '/about'){
+    res.end('Welcome To about page');
+ }
+ if(req.url === "/"){
+    res.end("welcome to to homepage")
+ }
+ res.end( `
+    <h1>Oops something went wrong </h1>
+    <a href="/" >Back To Homepage</a>
+    `)
 })
 
 server.listen(5000)
